3ae778a59274e57caf1e506423acd15df3fb2291
Kye <kye@apacmediasolutions.com>
2023-08-18 10:35:03 -0400

    dim head change + example change

--- example.py
+++ example.py
@@ -1,13 +1,17 @@
 import torch 
 from rt2.model import RT2
 
-rt2 = RT2()
+model = RT2()
 
 video = torch.randn(2, 3, 6, 224, 224)
+
 instructions = [
-    "bring me an apple on that tree"
+    'bring me that apple sitting on the table',
+    'please pass the butter'
 ]
 
-train_logits = rt2(video, instructions)
-rt2.eval()
-eval_logits = rt2(video, instructions, cond_scale=2)
+train_logits = model(video, instructions) # (2, 6, 11, 256) # (batch, frames, actions, bins)
+
+model.eval()
+
+eval_logits = model(video, instructions, cond_scale = 3.) # classifier free guidance with conditional scale of 3
